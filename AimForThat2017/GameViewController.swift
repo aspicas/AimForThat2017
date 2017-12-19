9c05c5ebe6d5a6f27bb5c7f7dd530c5e0580db9d
David Garcia <david.a.g.r93@gmail.com>
2017-12-19 13:57:24 -0400

    Pop up al pulsar Hit me para informar al jugador

--- AimForThat2017/GameViewController.swift
+++ AimForThat2017/GameViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hola mundo", message: "Este es mi juego Aim For That!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Genial", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true)
+    }
+    
 }
 
